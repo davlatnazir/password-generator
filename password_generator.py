75740c0ad7fb953a9e062c60edd014ce8ad5fdf5
davlatnazir <154667000+davlatnazir@users.noreply.github.com>
2026-02-15 17:03:58 +0500

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,10 @@
+import random 
+import string
+
+length = int(input(" Parol uzunligini kiriting: " ))
+
+characters = string.ascii_letters + string.digits + string.punctuation 
+
+password = ''.join(random.choice(characters) for i in range(length))
+
+print("Yaratilgan parol:",password)

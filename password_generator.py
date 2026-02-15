077c0f7bb89be8dfb817e5cbda82d17e8621e33c
davlatnazir <154667000+davlatnazir@users.noreply.github.com>
2026-02-15 17:20:59 +0500

    Updated project

--- password_generator.py
+++ password_generator.py
@@ -8,3 +8,4 @@ characters = string.ascii_letters + string.digits + string.punctuation
 password = ''.join(random.choice(characters) for i in range(length))
 
 print("Yaratilgan parol:",password)
+print("Updated by Davlatnazir")
